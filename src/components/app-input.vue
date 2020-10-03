e5404fa6723417337a9a4aa136d499d178853abf
Alfred Cepeda <AlfredJCepeda@gmail.com>
2020-10-03 00:28:56 -0400

    Adding dist folder for gitpages

---
+++ src/components/app-input.vue
@@ -0,0 +1,32 @@
+<template>
+  <main>
+    <div v-if="$slots.leftIcon" class="icon">
+      <slot name="leftIcon"></slot>
+    </div>
+
+    <input placeholder="Search for a country..." />
+  </main>
+</template>
+
+<script>
+export default {
+  name: 'app-input'
+}
+</script>
+
+<style lang="sass" scoped>
+  main
+    display: inline-flex
+    align-items: center
+
+  main, input
+    color: var(--text-input)
+    background-color: var(--element-bg-color)
+
+  input
+    width: 100%
+    border: 0
+
+  .icon
+    padding: 10px 25px
+</style>
